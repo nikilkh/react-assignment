8337ef953783210e86fa0042a046294f9e9c7235
nikilkh <nikilkh241295@gmail.com>
2022-08-09 13:58:42 +0530

    NH | fixed no result component design

--- src/styles/NoResult.styled.js
+++ src/styles/NoResult.styled.js
@@ -23,29 +23,23 @@ export const NoResultStyle = styled.div`
   @media only screen and (max-width: 1024px) {
     width: auto;
   }
-  
 
   img {
     width: 620px;
     height: 447px;
 
     @media only screen and (max-width: 375px) {
-    width: 200px;
-    height: 200px;
-
-  }
-    
+      width: 200px;
+      height: 200px;
+    }
   }
 `;
 
 export const ErrorHeadingStyle = styled.h2`
-margin-top: 0px;
-margin-bottom: 48px;
+  margin-top: 0px;
+  margin-bottom: 48px;
 
-@media only screen and (max-width: 375px) {
-    font-size:20px;
-    
-
+  @media only screen and (max-width: 375px) {
+    font-size: 20px;
   }
-
-`
+`;
