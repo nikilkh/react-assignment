5d04ebdb5404fd44fa35b10e105cf88dd064946b
nikilkh <nikilkh241295@gmail.com>
2022-08-09 14:26:57 +0530

    NH | fixed noresult component design

--- src/styles/NoResult.styled.js
+++ src/styles/NoResult.styled.js
@@ -6,19 +6,28 @@ export const NoResultStyle = styled.div`
   font-size: 32px;
   font-weight: 600;
   margin-top: 96px;
+ margin: auto auto;
   width: 1440px;
   height: 1024px;
-  border: 2px solid black;
 
   @media only screen and (max-width: 375px) {
-    width: auto;
+    width: 375px;
+    height: 640px;
+    margin-top: 24px;
+  }
+  @media only screen and (max-width: 425px) {
+    width: 425px;
+    height: 640px;
     margin-top: 24px;
   }
   @media only screen and (max-width: 600px) {
     width: auto;
+    margin-top: 24px;
   }
   @media only screen and (max-width: 768px) {
     width: auto;
+    margin-top: 28px;
+
   }
   @media only screen and (max-width: 1024px) {
     width: auto;
@@ -29,9 +38,20 @@ export const NoResultStyle = styled.div`
     height: 447px;
 
     @media only screen and (max-width: 375px) {
-      width: 200px;
-      height: 200px;
+      width: 240px;
+      height: 224px;
+      margin-top: 24px;
     }
+    @media only screen and (max-width: 425px) {
+      width: 410px;
+      height: 224px;
+      margin-top: 24px;
+    }
+    @media only screen and (max-width: 768px) {
+    margin-top: 24px;
+
+  }
+    
   }
 `;
 
@@ -40,6 +60,16 @@ export const ErrorHeadingStyle = styled.h2`
   margin-bottom: 48px;
 
   @media only screen and (max-width: 375px) {
+    font-size: 15px;
+  }
+
+  @media only screen and (max-width: 425px) {
     font-size: 20px;
+
+  }
+  @media only screen and (max-width: 768px) {
+    font-size: 22px;
+    
+
   }
 `;
